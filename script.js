5176604a2c1aa568f4deb1a7556da906690aad77
EmmanuelNyameke <155826955+EmmanuelNyameke@users.noreply.github.com>
2024-04-10 20:48:46 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// const news = document.getElementById("dropBtn");
+// const newsItems = document.getElementById("drop-links");
+// news.addEventListener("click", () => {
+//     newsItems.classList.add(".drop");
+// });
+
+
+const accessContent = document.getElementById("access");
+const showContent = document.getElementById("show");
+accessContent.addEventListener("click", () => {
+    showContent.style.display = "block";
+});
